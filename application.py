74e0074052a242ebeb1192f35f7f96aa61083dc3
yshanukajay <yshanukajay@gmail.com>
2026-04-12 10:10:03 +0530

    deployment to Elastic Beanstalk configuration

---
+++ application.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, request
+import numpy as np
+import pandas as pd
+
+from sklearn.preprocessing import StandardScaler
+from src.pipelines.prediction_pipeline import CustomData, PredictPipeline
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['GET', 'POST'])
+def predict():
+    if request.method == 'GET':
+        return render_template('home.html')
+    else:
+        data = CustomData(
+            gender=request.form.get('gender'),
+            race_ethnicity=request.form.get('race_ethnicity'),
+            parental_level_of_education=request.form.get('parental_level_of_education'),
+            lunch=request.form.get('lunch'),
+            test_preparation_course=request.form.get('test_preparation_course'),
+            reading_score=int(request.form.get('reading_score')),
+            writing_score=int(request.form.get('writing_score'))
+        )
+
+        df=data.get_data_as_data_frame()
+        print(df)
+
+        predict_pipeline=PredictPipeline()
+        result=predict_pipeline.predict(df)
+        print(result)
+
+        return render_template('home.html', result=result[0])
+    
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
