5c8af44d2462590712769378d7ac728aa4da1d8f
yshanukajay <yshanukajay@gmail.com>
2026-04-12 10:02:20 +0530

    flask app created

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -20,6 +20,15 @@ def save_object(file_path, obj):
         raise CustomException(e, sys)
     
 
+def load_object(file_path):
+    try:
+        with open(file_path, 'rb') as file_obj:
+            return pickle.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+
 def evaluate_models(X_train, y_train, X_test, y_test, models, params):
     try:
         report = {}
